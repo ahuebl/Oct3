d041025e8292afe9d75f6f8fc4c9c0bebb286ada
Allyson <32317454+ahuebl@users.noreply.github.com>
2017-10-03 12:39:31 -0700

    Added buttons & text fields

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,21 @@
+/* Use for a 
+   multi-line comment in js */
+
 // Simple function to test our setup
 function test() {
     alert("Hello World");
+}
+
+function readText () {
+    // Grab the input element from the document
+    let inputItem = document.getElementById("stuff");
+    // Grab the text from the input element
+    let theText = inputItem.value;
+    // Demonstrate...
+    alert("value is " + theText);
+    let theTextUpper = theText.toUpperCase();
+    // inputItem.value = theTextUpper;
+    // Grab the output element from the document
+    let outputItem = document.getElementById("stuff2");
+    outputItem.value = theTextUpper;
 }
