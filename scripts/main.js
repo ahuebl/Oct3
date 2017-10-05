325542bad41eaea220f1b7c6b9f812bc2b444f38
Allyson <allysonhuebl@cocc.edu>
2017-10-05 15:02:43 -0700

    Added Input & Output Text Areas
    & copied input into output with a button

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,29 @@
 /* Use for a 
    multi-line comment in js */
 
+function copyInputToOutput() {
+    let inputText = getEleText("inputText");
+    // let inputElement = document.getElementById("inputText");
+    // let inputText = inputElement.value;
+    // alert(inputText);
+    let outputElement = document.getElementById("outputText");
+    outputElement.value = inputText;
+}
+
+// Convenience function to get HTML element
+function getE(id) {
+    return document.getElementById(id);
+}
+
+// Return the value property of an HTML element
+// Pass in the element ID as a string
+function getEleText(id) {
+    return getE(id).value;
+}
+
 // Simple function to test our setup
 function test() {
-    alert("Hello World");
+    alert("Testing getEleText(): " + getEleText("stuff"));
 }
 
 function readText () {
